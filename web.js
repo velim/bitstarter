698a77bc65c7c196fe4c72520dfecce0c6fbfcdb
velim <ubuntu@ip-172-31-1-36.eu-west-1.compute.internal>
2013-07-07 22:17:53 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString('utf8'));
 });
